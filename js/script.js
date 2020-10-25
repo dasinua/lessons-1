ce306f71dece90b9a0993e8fe98fdb58b81540ec
Aleksandr <dasinua@gmai.com>
2020-10-25 10:27:29 +0200

    function

--- js/script.js
+++ js/script.js
@@ -1,21 +1,60 @@
 'use strict';
 
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
-const film = prompt('Один из последних просмотренных фильмов?', ''),
-      rating = prompt('На сколько оцените его?', ''),
-      film2 = prompt('Один из последних просмотренных фильмов?', ''),
-      rating2 = prompt('На сколько оцените его?', '');
+let numberOfFilms; 
+let film, rating;
 
 const personalMovieDB =  {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    gentres: [],
-    privat: false
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  gentres: [],
+  privat: false
 };
 
-personalMovieDB.movies[film] = rating;
-personalMovieDB.movies[film2] = rating2;
+function start()  {
+  numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+  while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)){
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+  }
+
+}
+
+start();
+
+function rememberMyFilms() {
+  for (let i = 0; i < 2; i++){
+    film = prompt('Один из последних просмотренных фильмов?', '');
+    rating = prompt('На сколько оцените его?', '');
+
+    if(film.length > 0 && film.length < 50){
+      personalMovieDB.movies[film] = rating;
+    } else {
+        i--;
+    }
+  }
+}
+
+rememberMyFilms();
+
+function showMyDB(hidden){
+  if(!hidden){
+    console.log(personalMovieDB);
+  }
+}
+
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+  for(let i = 2; i <= 3; i++){
+    const genre = prompt(`Ваш любимый жанр под номером ${i}`);
+    personalMovieDB.genres[i - 1] = genre;
+  }
+}
+
+writeYourGenres();
 
 
-console.log(personalMovieDB);
+
+
+
